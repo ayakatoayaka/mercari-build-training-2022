bdf976551693cae65fc65148efdd8bde925c50e9
Ayaka Fujimoto <fujimotoayaka.happy@gmail.com>
2022-05-19 21:05:05 +0900

    Update main.py for step3-6&3-7

--- python/main.py
+++ python/main.py
@@ -3,8 +3,9 @@ import logging
 import pathlib
 import json
 import sqlite3
+import hashlib
 from unicodedata import category
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -26,29 +27,37 @@ def existOrNot():
     c = conn.cursor()
     with open("../db/items.db",'r') as f:
         schema = f.read()
-
     if not c.fetchone():
         c.execute(f"""{schema}""")
-
     conn.commit()
     conn.close()
 
+def transDictList(itemsList):
+    itemDictList = []
+    for item in itemsList:
+        itemDictList.append({"name":item[1],"category":item[2],"image":item[3]})
+    return itemDictList
+
+def transDict(item):
+    print(item)
+    return {"name":item[1],"category":item[2],"image":item[3]} if item != None else {}
 
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...),category: str = Form(...)):
+def add_item(name: str = Form(...),category: str = Form(...),image: bytes = File(...)):
     existOrNot()
     conn = sqlite3.connect("../db/mercari.sqlite3")
     c = conn.cursor()
     c.execute("SELECT COUNT(*) FROM items")
     colums_num = c.fetchone()[0]+1
-    c.execute("INSERT INTO items VALUES(?, ?, ?)",(colums_num,name,category))
+    image_hash = hashlib.sha256(str(image).encode()).hexdigest()
+    c.execute("INSERT INTO items VALUES(?, ?, ?, ?)",(colums_num,name,category,image_hash))
     conn.commit()
     conn.close()
-    return colums_num,name,category
+    return colums_num,name,category,image_hash
 
 @app.get("/items")
 async def get_items():
@@ -57,9 +66,22 @@ async def get_items():
     c = conn.cursor()
     c.execute("SELECT * FROM items")
     items = c.fetchall()
+    items_dict = transDictList(items)
     conn.commit()
     conn.close()
-    return items
+    return items_dict
+
+@app.get("/items/{item_id}")
+async def get_items(item_id):
+    existOrNot()
+    conn = sqlite3.connect("../db/mercari.sqlite3")
+    c = conn.cursor()
+    c.execute("SELECT * FROM items WHERE id = ?",(item_id,))
+    item = c.fetchone()
+    item_dict = transDict(item)
+    conn.commit()
+    conn.close()
+    return item_dict
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
@@ -83,6 +105,7 @@ async def search_item(keyword: str):
     #https://www.ravness.com/posts/pythonsqlite
     c.execute("SELECT * FROM items WHERE name LIKE ? ", ('%'+keyword+'%',))
     items = c.fetchall()
+    items_dict = transDictList(items)
     conn.commit()
     conn.close()
-    return items
+    return {"items":items_dict}
