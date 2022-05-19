8ac0d77a45b718362bf226fc0327ed1f5d7f04d3
Ayaka Fujimoto <fujimotoayaka.happy@gmail.com>
2022-05-19 16:19:54 +0900

    Update step3-4&3-5

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ import os
 import logging
 import pathlib
 import json
+import sqlite3
 from unicodedata import category
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
@@ -20,31 +21,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def existOrNot():
+    conn = sqlite3.connect("../db/mercari.sqlite3")
+    c = conn.cursor()
+    with open("../db/items.db",'r') as f:
+        schema = f.read()
+
+    if not c.fetchone():
+        c.execute(f"""{schema}""")
+
+    conn.commit()
+    conn.close()
+
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
 def add_item(name: str = Form(...),category: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    with open('items.json') as f:
-        try: 
-            itemData = json.load(f)
-        except:
-            itemData = {"items":[]}
-    itemData["items"].append({"name": f"{name}", "category": f"{category}"})
-    with open('items.json', 'w') as f:
-        json.dump(itemData, f)
-    return {"message": f"item received: {name}"}
+    existOrNot()
+    conn = sqlite3.connect("../db/mercari.sqlite3")
+    c = conn.cursor()
+    c.execute("SELECT COUNT(*) FROM items")
+    colums_num = c.fetchone()[0]+1
+    c.execute("INSERT INTO items VALUES(?, ?, ?)",(colums_num,name,category))
+    conn.commit()
+    conn.close()
+    return colums_num,name,category
 
 @app.get("/items")
 async def get_items():
-    with open('items.json') as f:
-        try: 
-            itemData = json.load(f)
-        except:
-            itemData = {"items":[]}
-    return itemData
+    existOrNot()
+    conn = sqlite3.connect("../db/mercari.sqlite3")
+    c = conn.cursor()
+    c.execute("SELECT * FROM items")
+    items = c.fetchall()
+    conn.commit()
+    conn.close()
+    return items
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
@@ -59,3 +74,15 @@ async def get_image(items_image):
         image = images / "default.jpg"
 
     return FileResponse(image)
+
+@app.get("/search")
+async def search_item(keyword: str):
+    existOrNot()
+    conn = sqlite3.connect("../db/mercari.sqlite3")
+    c = conn.cursor()
+    #https://www.ravness.com/posts/pythonsqlite
+    c.execute("SELECT * FROM items WHERE name LIKE ? ", ('%'+keyword+'%',))
+    items = c.fetchall()
+    conn.commit()
+    conn.close()
+    return items
